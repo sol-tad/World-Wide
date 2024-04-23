2d9e1ac2cccfae1304a1b0be650207e7b6a39eeb
solabest <soltad65@gmail.com>
2024-04-23 03:45:06 -0700

    setup page routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      World Wise
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pricing() {
+  return (
+    <div>
+      Price
+    </div>
+  )
+}
+
+export default Pricing
