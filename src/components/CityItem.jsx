a42d08a6d0bb7d161e798c6dfd0c95f23e2295e6
solabest <soltad65@gmail.com>
2024-04-27 05:44:30 -0700

    add country detail and render it

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -11,10 +12,11 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   const { cityName, emoji, date,id,position } = city;
+  const{currentCity}=useCities();
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${id===currentCity.id?styles["cityItem--active"]:""}`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:3000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(function () {
     async function fetchCities() {
@@ -22,12 +23,25 @@ function CitiesProvider({ children }) {
     }
     fetchCities();
   }, []);
-
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error in loading  data ...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
   return (
     <CitiesContext.Provider
       value={{
         cities,
         isLoading,
+        currentCity,
+        getCity,
       }}
     >
       {children}
@@ -35,10 +49,11 @@ function CitiesProvider({ children }) {
   );
 }
 
-function useCities(){
-    const context=useContext(CitiesContext);
-    if(context===undefined) throw new Error("cities context was used out side the provider");
-    return context;
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("cities context was used out side the provider");
+  return context;
 }
 
-export {CitiesProvider,useCities};
+export { CitiesProvider, useCities };
