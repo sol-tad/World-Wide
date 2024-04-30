31b0fa9defe111be41e2ba343123e3aa7d4d6e1b
solabest <soltad65@gmail.com>
2024-04-30 04:56:15 -0700

    add latitude and longtude to the form

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
-  const navigate = useNavigate(); //navigate where ever we want
   const [searchParam, setSearchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const{cities}=useCities();
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
+  const { cities } = useCities();
+  const mapLat = searchParam.get("lat");
+  const mapLng = searchParam.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
   return (
     <div
       className={styles.mapContainer}
-      // onClick={() => {
-      //   navigate("form");
-      // }}
+      onClick={() => {
+        navigate("form");
+      }}
     >
       <MapContainer
         center={mapPosition}
@@ -39,9 +53,26 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
 
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate(); //navigate where ever we want
+
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng} `);
+    },
+  });
+}
 export default Map;
